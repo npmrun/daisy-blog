4e6756db3ba8990e50a87b1e917ffbf489410cbe
npmrun <kk1549469775@gmail.com>
2022-08-18 14:47:41 +0800

    3 files modified

--- src/data/nav.ts
+++ src/data/nav.ts
@@ -1,12 +1,13 @@
-export const title = 'Daisy blog';
-
+export const title = 'Dash Blog';
+export const avatar = 'https://xieyaxin.top/usr/uploads/2022/05/1432272769.jpg';
+// https://api.lorem.space/image/face?w=96&h=96
 export const nav = [
   {
-    title: 'About',
+    title: '关于',
     slug: '/about',
   },
   {
-    title: 'Contact',
+    title: '联系',
     slug: '/contact',
   },
 ];
