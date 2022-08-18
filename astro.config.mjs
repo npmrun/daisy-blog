49709f00be106a0ce38b11345dbaa89586ee075c
npmrun <kk1549469775@gmail.com>
2022-08-18 17:35:08 +0800

    4 files modified, 1 added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,26 @@
-import astroRemark from '@astrojs/markdown-remark';
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
+// https://github.com/withastro/astro/blob/main/examples/blog/src/pages/rss.xml.js
 // https://astro.build/config
 export default defineConfig(
   /** @type {import('astro').AstroUserConfig} */
   {
     markdown: {
-      render: [
-        astroRemark,
-        {
-          remarkPlugins: [],
-          rehypePlugins: [
-            'rehype-slug',
-            [
-              'rehype-autolink-headings',
-              {
-                behavior: 'wrap',
-              },
-            ],
-          ],
-          shikiConfig: {
-            theme: 'poimandres',
-            langs: [],
-            wrap: false,
+      remarkPlugins: [],
+      rehypePlugins: [
+        'rehype-slug',
+        [
+          'rehype-autolink-headings',
+          {
+            behavior: 'wrap',
           },
-        },
+        ],
       ],
+      shikiConfig: {
+        theme: 'poimandres',
+        langs: [],
+        wrap: false,
+      },
     },
     site: 'https://art.xieyaxin.top/',
     integrations: [sitemap()],
